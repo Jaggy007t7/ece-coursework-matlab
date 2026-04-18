069f0ae814334799fc2967c412c3d17285884b6c
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-04-18 21:23:06 +0530

    Create FM_mod_Demod.m

---
+++ Communication_system/FM_mod_Demod.m
@@ -0,0 +1,43 @@
+
+% Parameters
+fs = 1000;              % Sampling frequency (Hz)
+t = 0:1/fs:1;           % Time vector (1 second)
+fm = 5;                 % Message signal frequency (Hz)
+fc = 100;               % Carrier frequency (Hz)
+kf = 50;                % Frequency deviation (Hz)
+
+% Message Signal
+message = sin(2*pi*fm*t);
+
+% FM Modulation
+modulated = fmmod(message, fc, fs, kf);
+
+% FM Demodulation
+demodulated = fmdemod(modulated, fc, fs, kf);
+
+% Plotting
+figure;
+
+% Message Signal
+subplot(3,1,1);
+plot(t, message, 'b');
+title('Message Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+grid on;
+
+% FM Modulated Signal
+subplot(3,1,2);
+plot(t, modulated, 'r');
+title('FM Modulated Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+grid on;
+
+% Demodulated Signal
+subplot(3,1,3);
+plot(t, demodulated, 'g');
+title('Demodulated Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+grid on;
