82a3a8e9ca3a4d3bef099463968131513bf9b8e4
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-04-20 22:40:02 +0530

    Create Composite_trapezium_rule.m

---
+++ Numerical Methods of ECE/Integral/Double Integral/Composite_trapezium_rule.m
@@ -0,0 +1,33 @@
+clc;
+clear;
+% Double Integral using Composite Trapezium rule:-
+f = @(x,y) x+y;
+a=1;b=0; %Limits for x
+c=1;d=0; %Limits for y
+
+n1 =10; %Number of intervals for x;
+n2 =10; %Number of intervals for y;
+
+step_size_x = (a-b)/n1;
+step_size_y = (c-d)/n2;
+
+h = step_size_x;
+k = step_size_y;
+i=1;
+
+sum = 
+while(i<=n1-1)
+    j=1;
+       while(j<n2-1)
+            if()
+        j=j+1;
+       end
+
+
+i=i+1;
+end 
+   
+
+
+
+
