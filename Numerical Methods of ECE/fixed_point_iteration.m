561ba5a2e8c657f554e8f4156c70756783981936
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-04-24 18:40:10 +0530

    Update fixed_point_iteration.m

--- Numerical Methods of ECE/fixed_point_iteration.m
+++ Numerical Methods of ECE/fixed_point_iteration.m
@@ -17,3 +17,22 @@ x0 = x1;
 end
 
 fprintf('%.6f',x0);
+
+% another code;
+clc;
+clear;
+% Fixed point iteration method;
+g = @(x) (1-x)^(1/3);   % <-- Go for hit and trial in this case,
+% Initial approximation;
+a = 0.5;
+tol = 1e-6;
+
+for iter=1:50
+    y = g(a);
+    if(abs(a-y)<tol)
+        break;
+    end
+    a=y;
+end
+
+fprintf("%f %d",y,iter);
