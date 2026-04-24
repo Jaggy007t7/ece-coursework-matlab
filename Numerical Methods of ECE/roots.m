37d49700f9ccb5476884446c54448a7e482fac1f
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-04-24 15:16:15 +0530

    Update roots.m

--- Numerical Methods of ECE/roots.m
+++ Numerical Methods of ECE/roots.m
@@ -1,38 +1,22 @@
-% Bisection method:-
-f = @(x) x^2 -3*x+1;
-a = 0;
-b = 1;
-tol = 1e-6;
-max_iter = 50;
+% Root using bisection method;
+f = @(x) x^3-x-2;
+a=1;
+b=2;
+tol=1e-8;
 
-fa = f(a);
-fb = f(b);
+max_iter=50;
+for iter=0:max_iter
+    mid = (a+b)/2;
 
-if fa * fb >= 0
-    error('Function has same sign at interval endpoints.');
-end
-
-iter = 0;
-mid = (a+b)/2;  % initialize
-
-while iter < max_iter
-    mid = (a + b) / 2;
-    fmid = f(mid);
-    
-    if abs(fmid) < tol || (b - a) / 2 < tol
+    if(abs(a-b)<tol)
         break;
     end
-    
-    if fa * fmid < 0
-        b = mid;
-        fb = fmid;
-    else
-        a = mid;
-        fa = fmid;
-    end
-    
-    iter = iter + 1;
-end
 
-root = mid;
-fprintf('%f\n', root);
+    if(f(a)*f(mid)<0)
+        b=mid;
+    else
+        a=mid;
+    end
+end
+root =mid;
+fprintf("%f %f",mid,iter);
